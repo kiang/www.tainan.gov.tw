2c8b8c4c2e768a7edb6264f7c941696a1aaf1e40
kiang <kiange@gmail.com>
2020-12-15 21:22:08 +0800

    page index

---
+++ main.js
@@ -0,0 +1,21 @@
+let rightNow = new Date();
+let theDate = rightNow.toISOString().slice(0,10);
+let theYear = theDate.slice(0,4);
+let theMonth = theDate.slice(5,7);
+$(function() {
+    let metaFile = 'data/' + theYear + '/' + theDate + '.json';
+    $.getJSON(metaFile, {}, function(c) {
+        for(k in c) {
+            let dataFile = 'data/' + theYear + '/' + theMonth + '/' + theDate + '_' + k + '.json';
+            $.getJSON(dataFile, {}, function(d) {
+                let block = '<div class="col-md-12"><div class="card mb-12 shadow-sm"><div class="card-body">';
+                block += '<p class="card-text">' + d['title'] + '<pre>' + d['content'] + '</pre></p>';
+                block += '<div class="d-flex justify-content-between align-items-center"><div class="btn-group">';
+                block += '<a class="btn btn-sm btn-outline-secondary" href="' + d['url'] + '" target="_blank">連結</a>';
+                block += '</div></div>';
+                block += '</div></div></div>';
+                $('div.row').append(block);
+            })
+        }
+    })
+})
