09aed7dbac119197cb2f2d49d6b6f2de3d10e47a
auto commit <noreply@localhost>
2024-11-01 11:03:48 +0800

    auto update @ 2024-11-01 11:00:01

--- scripts/config_ex.php
+++ scripts/config_ex.php
@@ -1,4 +1,11 @@
 <?php
+/*
+token from https://developers.facebook.com/tools/explorer/
+permission: pages_show_list, pages_read_engagement, pages_manage_posts
+
+then extend the expire time here
+https://developers.facebook.com/tools/debug/accesstoken/
+*/
 return [
     'app_id' => '',
     'app_secret' => '',
